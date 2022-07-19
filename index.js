ac2de56358487098f1074687389d2f8b8a8beb8c
jacky <anhlt.bvh@gmail.com>
2022-07-19 23:40:18 +0700

    update

--- index.js
+++
@@ -1,3 +0,0 @@
-const jsonHelper = require('./src/helpers/json-helpers')
-let out = jsonHelper.readJson(process.cwd()+'/src/stub_response/get-booking.json')
-console.log(out)
--- settings.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    port: 5000,
-    hello_service_port: 5001,
-    customer_service_port: 5002
-}
